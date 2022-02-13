5cf56ea69b88635046d866f4c2abd46e1dfc7393
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-02-13 20:51:45 +0000

    feat(endpoint-media): show media in files view

--- packages/endpoint-media/lib/controllers/files.js
+++ packages/endpoint-media/lib/controllers/files.js
@@ -18,9 +18,51 @@ export const filesController = (application, publication) => ({
         throw new Error(response.__("errors.noDatabase.content"));
       }
 
+      const page = parseInt(request.query.page) || 1;
+      const limit = parseInt(request.query.limit) || 18;
+      const skip = (page - 1) * limit;
+
+      const files = await publication.media
+        .find()
+        .sort({ _id: -1 })
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+
+      const count = await publication.media.countDocuments();
+
+      // Pagination pages
+      const totalPages = Math.ceil(count / limit);
+      const nextPage = page < totalPages ? page + 1 : false;
+      const previousPage = page > 0 ? page - 1 : false;
+
+      // Pagination items
+      const items = [...Array(totalPages).keys()].map((item) => ({
+        current: item + 1 === page,
+        href: `?page=${item + 1}`,
+        text: item + 1,
+      }));
+
+      // Pagination results
+      const resultsFrom = (page - 1) * limit + 1;
+      let resultsTo = resultsFrom - 1 + limit;
+      resultsTo = resultsTo > count ? count : resultsTo;
+
       response.render("files", {
         title: response.__("media.files.title"),
-        files: await publication.media.find().toArray(),
+        files,
+        items,
+        next: {
+          href: `?page=${nextPage}`,
+        },
+        previous: {
+          href: `?page=${previousPage}`,
+        },
+        results: {
+          from: resultsFrom,
+          to: resultsTo,
+          count,
+        },
         parentUrl: `${publication.mediaEndpoint}/files/`,
       });
     } catch (error) {
